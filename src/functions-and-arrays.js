f5f0beab4d4a7c61a31670f0538878f9b509287f
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-06-03 17:42:03 -0400

    iteration 4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,4 +1,3 @@
-
 // Iteration #1: Find the maximum
 function maxOfTwoNumbers(num1, num2) {
   if (num1 > num2) {
@@ -25,38 +24,42 @@ function findLongestWord(words) {
 
 
 // Iteration #3: Calculate the sum
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+//const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function sumNumbers(numbers) {
   let sumOfNum = 0;
-  
+
   for (let number of numbers) {
     sumOfNum += number;
   }
   return sumOfNum;
- }
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() { }
+function sum() {}
 
 
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+//const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() { }
+function averageNumbers(num) {
+  let sum = sumNumbers(num)
+  let avg = sum / num.length;
+  return avg
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength() {}
 
 // Bonus - Iteration #4.1
-function avg() { }
+function avg() {}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -73,14 +76,14 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() { }
+function uniquifyArray() {}
 
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() { }
+function doesWordExist() {}
 
 
 
@@ -99,7 +102,7 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() { }
+function howManyTimes() {}
 
 
 
@@ -127,7 +130,7 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() { }
+function greatestProduct() {}
 
 
 
